a252b663f8f71ed294fd89f8533644ea746c5f2c
Zamor <zamor2@wp.pl>
2016-04-04 09:52:41 +0200

    no username or password exception

--- login/login.js
+++ login/login.js
@@ -5,7 +5,9 @@ function log_in() {
     /*jezeli nie to wyswietla panel login w tedy loguje po danych*/
     window.login_data = $("#login_form").serializeArray();
     window.login = login_data[0].value.toUpperCase();
-    console.log(login_data);
+    if (window.login_data[1].value == "" && window.login == "" ){
+        window.login = ".";
+    }
     $(function () {
         var url = "http://" + window.login + ":" + window.login_data[1].value + "@system.fastdata.com.pl:4567/framework/rin/leady?";
         $.ajax(url,
@@ -13,10 +15,15 @@ function log_in() {
                 statusCode: {
                     401: function () {
                         console.log("nie autoryzowano");
+                        $("#login_error").css("display", "block");
+                        $("#password").val('');
+                        $("#username").val('');
+
 
                     },
                     200: function () {
                         console.log("autoryzowano");
+                        $("#login_error").css("display", "none");
 
 
                             $("#leeds-content").load('leadinfo/index.html');
